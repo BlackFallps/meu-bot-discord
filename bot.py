4c954bfd1ff530a9004700b61c094ac3f43c751f
BlackFallps <kauabrgamer123@gmail.com>
2026-06-19 01:53:01 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -97,8 +97,8 @@ class PainelFilaView(View):
                     canal_encontrado = canal
                     break
             if canal_encontrado:
-                await canal_encontrado.send(f"{member.mention} **Sua Vaga na Fazenda Gomes Girardi foi liberada! Procure os Gerentes.**")
-                await interaction.followup.send(f"✅ Vaga de {removido_nome} liberada!", ephemeral=True)
+                await canal_encontrado.send(f"{member.mention} **Sua Vaga na Fazenda Gomes Girardi foi liberada, Procure os Gerentes ou os Donos no Condado Pra ser Contratado!!**")
+                await interaction.followup.send(f"Vaga de {removido_nome} liberada ✅", ephemeral=True)
 
 # --- Eventos ---
 @bot.event
